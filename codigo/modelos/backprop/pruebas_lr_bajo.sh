06557e615c3c61558c8eaef6a1bfcefdf1a3fa01
Francisco Jose Aparicio Martos <paco2000am@gmail.com>
2022-06-01 12:22:08 +0200

    datos arquitectura grande

---
+++ codigo/modelos/backprop/pruebas_lr_bajo.sh
@@ -0,0 +1,20 @@
+funciones=(0)
+bits=(2 3 4 5 6 7 8)
+global=(0 1)
+database=(MNIST FMNIST)
+programa=backprop_qtp.py
+
+for k in "${database[@]}"
+do
+	for i in "${funciones[@]}"
+	do
+		for j in "${bits[@]}"
+		do
+			for z in "${global[@]}"
+			do
+				echo python $programa --dataset $k --modo $i --n-bits $j --global-quantization $z --epochs 30 --n-layers 5 --hidden-width 20
+				python $programa --dataset $k --modo $i --n-bits $j --global-quantization $z --epochs 30 --n-layers 5 --hidden-width 20 --lr 0.01
+			done
+		done
+	done
+done
--- codigo/modelos/script_cuantizacion.sh
+++ codigo/modelos/script_cuantizacion.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-cd backprop
-./pruebas.sh
-cd ..
-#cd dni
+#cd backprop
 #./pruebas.sh
 #cd ..
+cd dni
+python mnist_dni.py --dataset MNIST --n-layers 5 --hidden-width 20 --save-model
+python mnist_dni.py --dataset FMNIST --n-layers 5 --hidden-width 20 --save-model
+./pruebas.sh
+#cd ..
 
-cd feedbackAlignment
-./pruebas.sh
-cd ..
-cd HSIC
-source env.sh
-./pruebas.sh
+#cd feedbackAlignment
+#./pruebas.sh
+#cd ..
+#cd HSIC
+#source env.sh
+#./pruebas.sh
